c99e1c7db196b3442a792b0313a1f3a83fc6c66f
root <root@vashty.vashty>
2023-03-30 14:00:29 +0300

    .

--- app.py
+++ app.py
@@ -1,17 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_migrate import Migrate
 
-app = Flask(__name__)
+from flask_login import (
+    UserMixin,
+    login_user,
+    LoginManager,
+    current_user,
+    logout_user,
+    login_required,
+)
+
+login_manager = LoginManager()
+login_manager.session_protection = "strong"
+login_manager.login_view = "login"
+login_manager.login_message_category = "info"
+
+db = SQLAlchemy()
+migrate = Migrate()
+bcrypt = Bcrypt()
 
 
-# @app.route('/hello')
-# def hello():
-#     return 'Hello, World!'
+def create_app():
+    app = Flask(__name__)
 
+    app.secret_key = 'secret-key'
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
-# @app.route('/')
-# def index():
-#     return render_template("index.html")
+    login_manager.init_app(app)
+    db.init_app(app)
+    migrate.init_app(app, db)
+    bcrypt.init_app(app)
 
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    return app
