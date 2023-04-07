b4b4064812b0f502b7fc38b7dae8408df386965c
root <root@vashty.vashty>
2023-04-07 16:50:48 +0300

    manage.py

--- manage.py
+++ manage.py
@@ -1,19 +1,32 @@
-
 def deploy():
     """Run deployment tasks."""
+
+    # Import necessary modules
     from app import create_app, db
     from flask_migrate import upgrade, migrate, init, stamp
     from models import User
 
+    # Create the Flask app instance
     app = create_app()
+
+    # Push the app context to make it the current context
     app.app_context().push()
+
+    # Create the database tables
     db.create_all()
 
-    # migrate database to latest revision
+    # Initialize the migration directory
     init()
+
+    # Stamp the migration with the current revision
     stamp()
+
+    # Migrate the database to the latest revision
     migrate()
+
+    # Upgrade the database to the latest revision
     upgrade()
 
 
+# Call the deploy function to run the deployment tasks
 deploy()
