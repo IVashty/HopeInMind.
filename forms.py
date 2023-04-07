8d394b750561782898c1381f30d2a78da8c9e1a7
root <root@vashty.vashty>
2023-04-07 16:38:20 +0300

    forms.py

--- forms.py
+++ forms.py
@@ -6,25 +6,39 @@ from wtforms import (
     DateField,
     TextAreaField,
 )
-
 from flask_wtf import FlaskForm
-from wtforms.validators import InputRequired, Length, EqualTo, Email, Regexp, Optional
-import email_validator
+from wtforms.validators import (
+    InputRequired,
+    Length,
+    EqualTo,
+    Email,
+    Regexp,
+    Optional,
+)
 from flask_login import current_user
 from wtforms import ValidationError, validators
 from models import User
+import email_validator
 
 
-class login_form(FlaskForm):
+# Define the login form
+class LoginForm(FlaskForm):
+    """
+    Login form that extends FlaskForm.
+    """
+
     email = StringField(validators=[InputRequired(), Email(), Length(1, 64)])
     pwd = PasswordField(validators=[InputRequired(), Length(min=8, max=72)])
     # Placeholder labels to enable form rendering
-    username = StringField(
-        validators=[Optional()]
-    )
+    username = StringField(validators=[Optional()])
 
 
-class register_form(FlaskForm):
+# Define the register form
+class RegisterForm(FlaskForm):
+    """
+    Registration form that extends FlaskForm.
+    """
+
     username = StringField(
         validators=[
             InputRequired(),
@@ -32,7 +46,7 @@ class register_form(FlaskForm):
             Regexp(
                 "^[A-Za-z][A-Za-z0-9_.]*$",
                 0,
-                "Usernames must have only letters, " "numbers, dots or underscores",
+                "Usernames must have only letters, numbers, dots or underscores",
             ),
         ]
     )
@@ -42,14 +56,20 @@ class register_form(FlaskForm):
         validators=[
             InputRequired(),
             Length(8, 72),
-            EqualTo("pwd", message="Passwords must match !"),
+            EqualTo("pwd", message="Passwords must match!"),
         ]
     )
 
     def validate_email(self, email):
+        """
+        Validate if the email is already registered in the database.
+        """
         if User.query.filter_by(email=email.data).first():
             raise ValidationError("Email already registered!")
 
-    def validate_uname(self, uname):
+    def validate_username(self, username):
+        """
+        Validate if the username is already taken in the database.
+        """
         if User.query.filter_by(username=username.data).first():
             raise ValidationError("Username already taken!")
